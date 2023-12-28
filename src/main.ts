a36f3dc0403bc5d5e6ff08ac4d11c66222fe36ad
Rizqi Bagus Andrean <bagus.ok55@gmail.com>
2023-12-28 18:51:37 +0700

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { CustomInterceptors } from './common/custom.interceptor';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalInterceptors(new CustomInterceptors());
+
+  const config = new DocumentBuilder()
+    .setTitle('Topup Game')
+    .setDescription('The Topup Game API description')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter your access token',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
